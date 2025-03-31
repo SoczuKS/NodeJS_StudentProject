31933c5adbfddc9422f60e6b4c77c47e3fd9a06b
SoczuKS <soczynskik@gmail.com>
2025-03-31 03:16:04 +0200

    Prepare basic frontend structure

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ const app = express()
 const api = new API()
 const port = 3000
 
-app.get('/', (request, response) => {
-    response.send('Hello World!')
+app.set('views', './templates')
+app.set('view engine', 'ejs')
+app.use(express.static('./public'))
+
+const pages = ['main', 'wiki', 'marketplace', 'signin', 'signup']
+
+app.get('/', async (request, response) => {
+    console.log("New request received with params: ", request.query)
+
+    let page = request.query.page
+    if (!page || !pages.includes(page)) {
+        console.log("As no page was provided, defaulting to main")
+        page = 'main'
+    }
+
+    try {
+        response.render('index', {language: 'pl', page: page})
+    } catch(error) {
+        console.error('Błąd odczytu pliku: ', error)
+        response.writeHead(500, { 'Content-Type': 'text/plain; charset=UTF-8' })
+        response.end()
+    }
 })
 
 app.get('/api', (request, response) => {
